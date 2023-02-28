5d753b5d5a711676bb81c5893c944ecd83a674a2
Satheesh CS <119049837+satheeshcs14@users.noreply.github.com>
2023-02-28 17:21:37 +0530

    Update app.py
    change waitresss

--- app.py
+++ app.py
@@ -13,6 +13,18 @@ def get():
     
     
     
+mode = "dev" 
+mode = "pro"
+ 
     
-if __name__ =="__main__":
-    app.run(host="127.0.2.1",   port=8000)
+   
+if __name__=="__main__":
+    from waitress import serve                #    waitress is the serve useing for WSGI(web serve gateway interface )
+    
+    if mode =="dev":                          #         dev           or                  "pro"
+
+        app.run(host="127.0.0.1",port=8000 ,debug=True)
+
+    else:
+
+        serve(app , host ="127.0.0.1", port=8000 )  
